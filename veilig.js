52bc031a399026dd80a1d73edd0916867dc01f4e
Marcel Akerboom <makerboom@mborijnland.nl>
2024-09-27 13:07:04 +0200

    Initial commit

---
+++ veilig.js
@@ -0,0 +1,30 @@
+const API_URL = "https://my-json-server.typicode.com/makerboomRijnland/lms-veilig-typicode";
+
+class Contact {
+    static resource = '/contacts';
+
+    constructor(properties = {}) {
+        this.name = properties.name;
+        this.phone = properties.phone;
+    }
+
+    static async getAll() {
+        const url = `${API_URL}/${this.resource}`;
+
+        const response = await fetch(url);
+        if(!response.ok) {
+            throw new Error(`Failed to fetch contacts: ${response.status}`);
+        }
+        const result = await response.json();
+        const contacts = [];
+        
+        for(const object of result) {
+            const contact = new Contact(object);
+            contacts.push(contact);
+        }
+
+        return contacts;
+    }
+}
+
+const contacts = await Contact.getAll();
